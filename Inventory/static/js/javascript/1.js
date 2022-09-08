ba5400dadc254eadf23af07c4e223070f37edf57
IT20097042 <87925907+IT20097042@users.noreply.github.com>
2022-09-08 13:17:40 +0530

    Implementing the charts and graph

---
+++ Inventory/static/js/javascript/1.js
@@ -0,0 +1,13 @@
+<script type="text/javascript"> 
+function display_c(){
+var refresh=1000; // Refresh rate in milli seconds
+mytime=setTimeout('display_ct()',refresh)
+}
+
+function display_ct() {
+var x = new Date()
+document.getElementById('ct').innerHTML = x;
+display_c();
+ }
+
+</script>
