3a21f30af0f5371df05d0b2139b851dd2037d9df
Greshan Warnakulasuriya <greshan@ninodigitalhouse.com>
2022-09-08 20:02:31 +0530

    Developed patient list and add patients functions

--- patients/models.py
+++ patients/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import date
 
 class Patient(models.Model):
     name = models.CharField(max_length=120, unique=True)
@@ -7,7 +8,14 @@ class Patient(models.Model):
     NIC = models.CharField(max_length=12)
     email = models.CharField(max_length=120)
     address = models.CharField(max_length=220)
+    qr = models.CharField(max_length=200)
     created_date = models.DateField(auto_now_add=True)
+
+    def age(self):
+        today = date.today()
+        return today.year - self.dob.year - ((today.month, today.day) < (self.dob.month, self.dob.day))
+
+    age = property(age)
 
     def __str__(self):
         return self.name
--- patients/views.py
+++ patients/views.py
@@ -1,14 +1,31 @@
 from multiprocessing import context
-from django.shortcuts import render
-from .forms import PatientForm
+import secrets
+from this import d
+from django.shortcuts import render, redirect
+
+from patients.models import Patient
 
 # Create your views here.
 def add_patient(request):
-    forms = PatientForm()
-    context = {
-        'form' : forms,
-    }
-    return render(request, 'add_patient.html', context)
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        dob = request.POST.get('dob')
+        nic = request.POST.get('nic')
+        email = request.POST.get('email')
+        address = request.POST.get('address')
+        qr = secrets.token_hex(16)
+
+        patient = Patient(name=name,phone=phone ,dob=dob ,NIC=nic ,email=email ,address=address, qr=qr)
+        patient.save()
+        return redirect('patients')
+        
+
+    return render(request, 'add_patient.html')
 
 def patients(request):
-    return render(request, 'patients.html')
+    patients = Patient.objects.all()
+
+    return render(request, 'patients.html', {
+        'patients': patients
+    })
