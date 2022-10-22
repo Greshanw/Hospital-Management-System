3cd625ee0c41245e4742f2d32dcc272df169ce47
IT19018560 <88573195+IT19018560@users.noreply.github.com>
2022-10-22 20:38:12 +0530

    final commit

--- appoinment/models.py
+++ appoinment/models.py
@@ -4,10 +4,10 @@ from datetime import date,time
 
 
 class Appoinment(models.Model):
-    Doctor_Name = models.CharField(max_length=120, unique=True)
-    Patient_Name = models.CharField(max_length=120, unique=True)
-    Date = models.DateField()
-    Time = models.TimeField()
+    Doctor_Name = models.CharField(max_length=120, blank=False, null=False)
+    Patient_Name = models.CharField(max_length=120, blank=False, null=False)
+    Date = models.DateField(auto_now_add=True)
+    Time = models.TimeField(auto_now_add=True)
 
     def __str__(self):
             return self.Doctor_Name
